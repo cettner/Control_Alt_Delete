65b03e7468d67aa5990a181c514d886f2586f4bf
cettner <cettner@carthage.edu>
2018-12-17 23:07:41 -0600

    Adding Commander Class
    Adding empty class to start on the FPS development

---
+++ Source/RTS_Project/Commander.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RTSMinion.h"
+#include "Camera/CameraComponent.h"
+#include "Commander.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_PROJECT_API ACommander : public ARTSMinion
+{
+	GENERATED_BODY()
+	
+
+public:
+	ACommander();
+	UCameraComponent* FPS_Camera;
+	
+	
+	
+};
