1df518d71abb5becb90f858b32a15bb96c859db0
cettner <cettner@carthage.edu>
2019-06-06 23:05:37 -0500

    Fixing Mismatched Materials
    Also Added Game Mode class

---
+++ Source/RTS_Project/Public/DefaultMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "DefaultMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_PROJECT_API ADefaultMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
