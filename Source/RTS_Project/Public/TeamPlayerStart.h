c0eb20f8aa4c4186ecc47c8d0db182a374c00434
cettner <cettner@carthage.edu>
2019-08-04 20:11:02 -0500

    Added TeamPlayer Start

---
+++ Source/RTS_Project/Public/TeamPlayerStart.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "TeamPlayerStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_PROJECT_API ATeamPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
