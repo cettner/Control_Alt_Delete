664a5eed1bbb3c2bc9821499b642868072964683
cettner <cettner@carthage.edu>
2020-05-31 22:10:00 -0500

    Initial Session Interface Framework

--- Source/RTS_Project/GameLifetime/LobbyGameInstance.cpp
+++ Source/RTS_Project/GameLifetime/LobbyGameInstance.cpp
@@ -2,4 +2,90 @@
 
 
 #include "LobbyGameInstance.h"
+#include "OnlineSessionSettings.h"
+#include "OnlineSubsystemTypes.h"
 
+const static FName SESSION_NAME = TEXT("CoopPuzzleGameSession");
+const static FName SERVER_NAME_SETTINGS_KEY = TEXT("ServerName");
+
+void ULobbyGameInstance::Host(FString ServerName)
+{
+	DesiredServerName = ServerName;
+
+	if (SessionInterface.IsValid())
+	{
+		// Checks for an existing session
+		auto ExistingSession = SessionInterface->GetNamedSession(SESSION_NAME);
+
+		if (ExistingSession != nullptr)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("[ULobbyGameInstance::Host] There is an existing session about to remove the current one"));
+
+			SessionInterface->DestroySession(SESSION_NAME);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("[ULobbyGameInstance::Host] About to create session"));
+
+			// Create a new session
+			CreateSession();
+
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[ULobbyGameInstance::Host] SessionInterface invalid"));
+	}
+}
+
+void ULobbyGameInstance::JoinSession(uint32 Index)
+{
+	if (!SessionInterface.IsValid() || (!SessionSearch.IsValid())) return;
+
+	if (Index < (uint32)(SessionSearch->SearchResults.Num()))
+	{
+		SessionInterface->JoinSession(0, SESSION_NAME, SessionSearch->SearchResults[Index]);
+	}
+}
+
+void ULobbyGameInstance::OpenSessionListMenu()
+{
+//	if (MainMenu == nullptr) return;
+
+	SessionSearch = MakeShareable(new FOnlineSessionSearch());
+
+	if (SessionSearch.IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[ULobbyGameInstance::OpenSessionListMenu] Session is valid"));
+		//SessionSearch->bIsLanQuery = true;
+		SessionSearch->MaxSearchResults = 100;
+		SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+		SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+	}
+}
+
+void ULobbyGameInstance::OnCreateSessionComplete(FName SessionName, bool Success)
+{
+}
+
+void ULobbyGameInstance::OnDestroySessionComplete(FName SessionName, bool Success)
+{
+}
+
+void ULobbyGameInstance::OnFindSessionsComplete(bool Success)
+{
+}
+
+void ULobbyGameInstance::OnJoinSessionsComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void ULobbyGameInstance::CreateSession()
+{
+}
+
+
+void ULobbyGameInstance::EndSession()
+{
+
+}
--- Source/RTS_Project/GameLifetime/LobbyGameInstance.h
+++ Source/RTS_Project/GameLifetime/LobbyGameInstance.h
@@ -5,19 +5,44 @@
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "OnlineSubsystem.h"
+#include "OnlineSessionInterface.h"
+#include "PreGame/LobbySystem/SessionMenuInterface.h"
 #include "LobbyGameInstance.generated.h"
+
 
 
 /**
  * 
  */
 UCLASS()
-class RTS_PROJECT_API ULobbyGameInstance : public UGameInstance
+class RTS_PROJECT_API ULobbyGameInstance : public UGameInstance, public ISessionMenuInterface
 {
 	GENERATED_BODY()
 	
+public:
+	///// ISessionMenuInterface /////////////////// 
+	UFUNCTION()
+	void Host(FString ServerName) override;
 
+	UFUNCTION()
+	void JoinSession(uint32 Index) override;
 
+	UFUNCTION()
+	void EndSession() override;
+
+	UFUNCTION()
+	void OpenSessionListMenu() override;
+	///// ISessionMenuInterface /////////////////// 
+
+private:
+	// Session Events
+	FString DesiredServerName;
+	void OnCreateSessionComplete(FName SessionName, bool Success);
+	void OnDestroySessionComplete(FName SessionName, bool Success);
+	void OnFindSessionsComplete(bool Success);
+	void OnJoinSessionsComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	void CreateSession();
 private:
 
 	// Session
