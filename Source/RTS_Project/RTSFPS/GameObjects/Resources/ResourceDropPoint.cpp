3e97bcf183c2cbd504991852cf0268c9eba92d25
cettner <chettner@gmail.com>
2022-01-03 22:03:11 -0600

    Drop Point Update Commit

--- Source/RTS_Project/RTSFPS/GameObjects/Resources/ResourceDropPoint.cpp
+++ Source/RTS_Project/RTSFPS/GameObjects/Resources/ResourceDropPoint.cpp
@@ -4,7 +4,11 @@
 #include "ResourceDropPoint.h"
 #include "DrawDebugHelpers.h"
 #include "Components/ShapeComponent.h"
+
 #include "RTS_Project/RTSFPS/Shared/Interfaces/ResourceGatherer.h"
+#include "RTS_Project/GameArchitecture/Game/RTFPSGameState.h"
+#include "RTS_Project/RTSFPS/BaseClasses/Interfaces/RTSObjectInterface.h"
+
 
 AResourceDropPoint::AResourceDropPoint() : Super()
 {
@@ -14,17 +18,25 @@ AResourceDropPoint::AResourceDropPoint() : Super()
 
 void AResourceDropPoint::OnOverlapBegin(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
-	IResourceGatherer * gatherer = Cast<IResourceGatherer>(OtherActor);
-
-	if (gatherer != nullptr)
+	if (HasAuthority())
 	{
-		FReplicationResourceMap resources = gatherer->GetAllHeldResources();
-
-		for (int i = 0; i < resources.Num(); i++)
+		IResourceGatherer * gatherer = Cast<IResourceGatherer>(OtherActor);
+		IRTSObjectInterface * rtsobject = Cast<IRTSObjectInterface>(OtherActor);
+		const UWorld * world = GetWorld();
+		
+		if (gatherer != nullptr)
 		{
-			gatherer->RemoveResource(resources[i].Key, resources[i].Value);
+			FReplicationResourceMap resources = gatherer->GetAllHeldResources();
+
+			for (int i = 0; i < resources.Num(); i++)
+			{
+				ARTFPSGameState * gs = world->GetGameState<ARTFPSGameState>();
+				gs->ScoreResource(resources[i].Key,resources[i].Value, rtsobject);
+				gatherer->RemoveResource(resources[i].Key, resources[i].Value);
+			}
 		}
 	}
+
 }
 
 void AResourceDropPoint::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
