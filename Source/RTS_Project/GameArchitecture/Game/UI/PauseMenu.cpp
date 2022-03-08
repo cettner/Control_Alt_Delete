4ab053cfbdb2c2831739c28730100147a2382570
cettner <chettner@gmail.com>
2022-03-08 07:59:27 -0600

    Adding Design Component to Pause Menu

--- Source/RTS_Project/GameArchitecture/Game/UI/PauseMenu.cpp
+++ Source/RTS_Project/GameArchitecture/Game/UI/PauseMenu.cpp
@@ -8,9 +8,14 @@ bool UPauseMenu::Initialize()
     bool Success = Super::Initialize();
 
     if (!Success) return false;
-
+	
+	if (ResumePlayButton == nullptr) return false;
     ResumePlayButton->OnClicked.AddDynamic(this, &UPauseMenu::OnResumePlayButtonPressed);
+
+	if (ReturnToLobbyButton == nullptr) return false;
     ReturnToLobbyButton->OnClicked.AddDynamic(this, &UPauseMenu::OnReturnToLobbyButtonPressed);
+
+	if (ExitToMainMenuButton == nullptr) return false;
     ExitToMainMenuButton->OnClicked.AddDynamic(this, &UPauseMenu::OnExitToMainMenuButtonPressed);
 
 
